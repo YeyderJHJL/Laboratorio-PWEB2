3ef36737f3fae3273ae1cc53c3a35b62e873be78
yeyderjhjl <jturpoan.@unsa.edu.pe>
2023-05-13 01:20:05 -0500

    Agrega ingreso y salida de nombre

---
+++ IngresoNombre.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+//Imprimir nombre
+
+public class ingresoNombre{
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Ingrese su nombre: ");
+		String nombre = sc.nextLine();
+		System.out.println("Hola "+nombre+", es un gusto tenerte en este curso");
+	
+	}
+}
+
